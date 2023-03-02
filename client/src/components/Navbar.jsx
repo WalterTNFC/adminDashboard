2540e5abeba79076e6f3187c6eecfa0f8329cb81
WalterTNFC <walter.tad@hotmail.com>
2023-03-02 11:24:40 -0300

    feat: Sidebar open and close

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,7 +24,10 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = (
+  isSidebarOpen,
+  setIsSidebarOpen,
+) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
@@ -39,7 +42,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/*Left Side*/}
         <FlexBetween>
-          <IconButton onClick={() => console.log('open/close sidebar')}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
