7827ff85c99d6b6eda6cd7ab3c70dded9d73ad98
WalterTNFC <walter.tad@hotmail.com>
2023-03-11 19:02:02 -0300

    feat: logs to debug

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -2,8 +2,11 @@ import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
   try {
-    const { id } = req.body();
+    console.log('Olá entrou no controller');
+    const { id } = req.params;
+    console.log(`Chegou na função getUser no controller ${id}`);
     const user = await User.findById(id);
+    console.log(user)
     res.status(200).json(user);
 
   } catch (error) {
