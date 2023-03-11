8281f3f502cee22d43f99cb2ff58574f6b747494
WalterTNFC <walter.tad@hotmail.com>
2023-03-11 19:01:37 -0300

    feat: get user data to send to sidebar and navbar

--- client/src/scenes/layout/index.js
+++ client/src/scenes/layout/index.js
@@ -10,13 +10,12 @@ const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
-  console.log(userId);
   const { data } = useGetUserQuery(userId);
-  console.log('data', data);
 
   return (
     <Box display={isNonMobile ? "flex" : "block" } width="100%" height="100%">
-      <Sidebar 
+      <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -24,6 +23,7 @@ const Layout = () => {
       />
       <Box>
         <Navbar
+        user={data || {}}
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
         />
