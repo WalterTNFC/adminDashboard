0417c68ff318da166875cdb93e5b79cebf75f8c3
WalterTNFC <walter.tad@hotmail.com>
2023-03-12 17:05:54 -0300

    fix: is open navbar, add user information and logout option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LightModeOutlined,
   DarkModeOutlined,
@@ -24,12 +24,14 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = (
-  isSidebarOpen,
-  setIsSidebarOpen,
-) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar 
@@ -70,6 +72,55 @@ const Navbar = (
           <IconButton>
             <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
+
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform:"none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                  component="img"
+                  alt="profile"
+                  src={profileImage}
+                  height="32px"
+                  width="32px"
+                  borderRadiux="50%"
+                  sx={{ objectFit: "cover"}}
+              />
+              <Box textAlign="left">
+                  <Typography
+                    frontWeight="bold"
+                    fontSize="0.8rem"
+                    sx={{ color: theme.palette.secondary[100] }}
+                  >
+                    {user.name}
+                  </Typography>
+                  <Typography
+                    fontSize="0.75rem"
+                    sx={{ color: theme.palette.secondary[200] }}
+                  >
+                    {user.occupation}
+                  </Typography>
+                </Box>
+                <ArrowDropDownOutlined
+                  sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+                />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "button", horizontal: "center" }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
